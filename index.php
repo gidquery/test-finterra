125749cf0d8de407262b51392c01154d5947158f
Anton_Shilov <shilov.a.a.1986@gmail.com>
2020-04-23 23:57:42 +0700

    check function

--- index.php
+++ index.php
@@ -1,18 +1,16 @@
 <?php
 
-require_once('core.php');// Подключает файл только один раз, в core часовой пояс, соединение с БД, подключение функций И тд.
-
+require_once('core_.php');// Подключает файл только один раз, в core часовой пояс, соединение с БД, подключение функций И тд.
 
 
 // Запрос для главной страницы
 $query_position = "SELECT position_name FROM position";
-$result_position = mysqli_query($mysqli_connect, $query_position);
+$result_position = mysqli_query($mysqli_connect, $query_position);//Выполняет запрос к базе данных, возвращает некое подготовленное выражение
 if ($result_position) {
-    $position = mysqli_fetch_all($result_position, MYSQLI_ASSOC);
+    $position = mysqli_fetch_all($result_position, MYSQLI_ASSOC);//Помещает данные из запроса в массив
 } else {
     show_error($mysqli_connect);
 }
-
 
 
 // Подключение шаблонов
@@ -20,7 +18,7 @@ $page_content = include_template(
     'main.php',
     [
         'position' => $position,
-      //  'categories' => $categories,
+        'result_position' => $result_position
     ]
 );
 
@@ -28,8 +26,7 @@ $layout_content = include_template(
     'layout.php',
     [
         'content' => $page_content,
-     //   'categories' => $categories,
-        'title' => 'Test - Главная страница',
+        'title' => 'Каталог - Главная страница',
         'user' => $user
     ]
 );
